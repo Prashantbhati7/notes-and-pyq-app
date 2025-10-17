bbf40164c1e4294891d4ffe7849225cfd2c1b409
Prashant Bhati <prashantbhati774@gmail.com>
2025-10-17 18:07:18 +0530

    Add cookie-parser package and update related files
    - Introduced cookie-parser version 1.4.7 with dependencies.
    - Added LICENSE, HISTORY.md, README.md, and index.js files for cookie-parser.
    - Updated package.json and package-lock.json to include cookie-parser.
    - Modified frontend Login and Signup components for better structure.

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -19,20 +19,20 @@ export default function Login(){
     return(<>
     <div className="flex h-[92%] flex-row items-center justify-center">
     <form className="w-[40%] mt-10 "> 
-       {/* <label htmlFor="email" >Email</label>
+       <label htmlFor="email" >Email</label>
        <input type="text" id="email" name="email" className="border mb-6 py-1 w-full rounded-md outline-red" />
 
        <label htmlFor="username">password</label>
        <input type="text" id="password" name="password" className="border py-1 mb-6 w-full rounded-md outline-red" />
        <button className="border ms-[42%] px-[5%] rounded-md hover:bg-blue-400" type="submit" onClick={handlesubmit}>Login</button>
        <hr  className="mt-4 "/>
-       <p className="text-center">or</p> */}
+       {/* <p className="text-center">or</p>
        <div className="flex flex-row justify-center">
         <button className="bg-blue-600 mt-4 w-full py-5 border rounded-xl text-center text-red-50" onClick={GoogleAuth}>Continue with google</button>
        </div>
        <div className="flex flex-row mt-3 justify-center">
-        <p>new here? <Link to='/signup' className="text-blue-700">SignUp</Link></p>
-        </div>
+        <p>new here? <Link to='/signup' className="text-blue-700">SignUp</Link></p> */}
+        {/* </div> */}
     </form> 
     </div>
     </>)
--- frontend/src/pages/Signup.jsx
+++ frontend/src/pages/Signup.jsx
@@ -18,9 +18,9 @@ export default function Signup(){
             window.open(`${import.meta.env.VITE_APP_API_URL}/auth/google`,
          'self',)
         }
-        return(<>
-        <div className="flex flex-col h-[92%] justify-center">
-        {/* <form className="w-[40%] mt-10"> 
+        return( <>
+        <div className="flex flex-col items-center h-[92%] justify-center">
+        <form className="w-[40%] mt-10"> 
     
            <label htmlFor="username" >username</label>
            <input type="text" id="username" name="username" className="border mb-6 w-full rounded-md outline-red" />
@@ -39,11 +39,13 @@ export default function Signup(){
            <button className="border ms-[40%] px-8 py-1 rounded-md hover:bg-blue-400" type="submit" onClick={handlesubmit}>signup</button>
         </form> 
         <hr className="mt-4"/>
-        <p className="text-center">or</p> */}
+        {/* <p className="text-center">or</p>
         <div className="flex w-full flex-row justify-center">
             <button className="bg-blue-600 w-[40%] py-3 border rounded-xl text-center text-blue-100" onClick={GoogleAuth}>continue with google</button>
         </div>
         <div className="text-center w-full mt-3">already have account? <Link to='/login' className="text-blue-700">Log in</Link></div>
+        </div> */}
         </div>
-        </>)
+        </>
+        )
 };
